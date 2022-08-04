9811c255c946f87c4efd22f6778d812ce8160f98
Patrizio Bekerle <patrizio@bekerle.com>
2022-08-04 10:44:31 +0200

    Add a parameter

--- src/Command/StatusTestCommand.php
+++ src/Command/StatusTestCommand.php
@@ -6,6 +6,7 @@ namespace Dbp\Relay\DispatchBundle\Command;
 
 use Dbp\Relay\DispatchBundle\Service\DispatchService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -30,13 +31,16 @@ class StatusTestCommand extends Command
      */
     protected function configure()
     {
-        $this->setDescription('Status test command');
+        $this
+            ->setDescription('Status test command')
+            ->addArgument('dispatch_request_identifier', InputArgument::OPTIONAL, 'dispatchRequestIdentifier', '4d553985-d44f-404f-acf3-cd0eac7ae9c2');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Creating status change...');
-        $this->dispatchService->createRequestStatusChange('4d553985-d44f-404f-acf3-cd0eac7ae9c2', 1, 'Test');
+        $dispatchRequestIdentifier = $input->getArgument('dispatch_request_identifier');
+        $this->dispatchService->createRequestStatusChange($dispatchRequestIdentifier, 1, 'Test');
 
         return 0;
     }
