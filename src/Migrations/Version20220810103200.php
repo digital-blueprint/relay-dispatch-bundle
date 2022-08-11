cd97ca673c47cf5c2bf2e0f24409e8665df035ca
Patrizio Bekerle <patrizio@bekerle.com>
2022-08-11 10:22:48 +0200

    Add DELETE and UPDATE cascades

---
+++ src/Migrations/Version20220810103200.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\DispatchBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add DELETE and UPDATE cascades.
+ */
+final class Version20220810103200 extends EntityManagerMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds DELETE and UPDATE cascades.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE dispatch_request_files DROP FOREIGN KEY dispatch_request_files_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_files ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier) ON DELETE CASCADE ON UPDATE CASCADE');
+
+        $this->addSql('ALTER TABLE dispatch_request_recipients DROP FOREIGN KEY dispatch_request_recipients_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_recipients ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier) ON DELETE CASCADE ON UPDATE CASCADE');
+
+        $this->addSql('ALTER TABLE dispatch_request_status_changes DROP FOREIGN KEY FK_83F6E7C899F83637');
+        $this->addSql('ALTER TABLE dispatch_request_status_changes DROP FOREIGN KEY dispatch_request_status_changes_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_status_changes ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier) ON DELETE CASCADE ON UPDATE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE dispatch_request_files DROP FOREIGN KEY dispatch_request_files_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_files ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier)');
+
+        $this->addSql('ALTER TABLE dispatch_request_recipients DROP FOREIGN KEY dispatch_request_recipients_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_recipients ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier)');
+
+        $this->addSql('ALTER TABLE dispatch_request_status_changes DROP FOREIGN KEY dispatch_request_status_changes_ibfk_1');
+        $this->addSql('ALTER TABLE dispatch_request_status_changes ADD FOREIGN KEY (dispatch_request_identifier) REFERENCES dispatch_requests(identifier)');
+    }
+}
