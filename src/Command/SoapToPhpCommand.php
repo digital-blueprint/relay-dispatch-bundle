d5a26ac250ad59fb4552a2c32a87f080b2f4310f
Patrizio Bekerle <patrizio@bekerle.com>
2022-08-25 15:00:02 +0200

    Add dbp:relay-dispatch:soap-to-php command

---
+++ src/Command/SoapToPhpCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\DispatchBundle\Command;
+
+use Dbp\Relay\DispatchBundle\Helpers\Tools;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command to convert a soap request to php code.
+ * ./console dbp:relay-dispatch:soap-to-php /application/DualDeliveryRequest_2020-09-11_17-14-06-1131.xml > /application/DualDeliveryRequest.php
+ */
+class SoapToPhpCommand extends Command
+{
+    protected static $defaultName = 'dbp:relay-dispatch:soap-to-php';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('SOAP code to PHP code converter')
+            ->addArgument('file_path', InputArgument::REQUIRED, 'file path');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Trying to convert SOAP code to PHP code...');
+        $filePath = $input->getArgument('file_path');
+
+        if (!file_exists($filePath)) {
+            $output->writeln('File not found: ' . $filePath);
+            return 1;
+        }
+
+        $soapString = file_get_contents($filePath);
+        $xmlString = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2___$3", $soapString);
+
+//        var_dump($soapString);
+        $xml = simplexml_load_string($xmlString);
+//        var_dump($xml);
+        $result = Tools::makeXmlCode($xml, '$xml', '', "\n", "    ");
+
+        print($result);
+
+        return 0;
+    }
+}
--- src/Helpers/Tools.php
+++ src/Helpers/Tools.php
@@ -13,4 +13,74 @@ class Tools
     {
         return 'data:'.$mime.';base64,'.base64_encode($data);
     }
+
+    public static function makeXmlCodeFromSoap(
+        $soapString,
+        $parentNodeName = '$xml',
+        $tabs = '',
+        $breakType = "\n",
+        $tabType = '    '
+    ): string
+    {
+        $xmlString = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2___$3", $soapString);
+        $xml = simplexml_load_string($xmlString);
+
+        return self::makeXmlCode($xml, $parentNodeName, $tabs, $breakType, $tabType);
+    }
+
+    public static function makeXmlCode(
+        $xmlData,
+        $parentNodeName = '$xml',
+        $tabs = '',
+        $breakType = "<br>",
+        $tabType = '&#9;',
+        $soapReplacement = '___'
+    ): string {
+        $start_tabs = $tabs;
+        $tabs = $tabs.$tabType;
+        $codeText = '';
+
+        if ($parentNodeName == '$xml') {
+            $codeText = '$xml = new \DOMDocument(\'1.0\',\'UTF-8\');'.$breakType;
+        }
+
+        foreach ($xmlData as $xmlvarname => $xmlnode) {
+            $attCodeText = '';
+            $nodeVal = '';
+            $nestedCodeText = '';
+
+            $xmlnodename = str_replace($soapReplacement, ':', $xmlvarname);
+            $xmlvarname = str_replace($soapReplacement, '', $xmlvarname);
+
+            if ($xmlnode->attributes()) {
+                foreach ($xmlnode->attributes() as $attname => $attvalue) {
+                    $attname = str_replace($soapReplacement, ':', $attname);
+                    $attCodeText .= $tabs.'$xml_'.$xmlvarname.'->setAttribute(\''.$attname.'\',\''.$attvalue.'\');'.$breakType;
+                }
+            }
+
+            if (trim($xmlnode->__toString())) {
+                $nodeVal = ',\''.trim($xmlnode->__toString()).'\'';
+            } elseif (count($xmlnode->children()) == 0) {
+                //empty child element
+            } else {
+                $nestedCodeText .= self::makeXmlCode(
+                    $xmlnode,
+                    '$xml_'.$xmlvarname,
+                    $tabs,
+                    $breakType,
+                    $tabType,
+                    $soapReplacement
+                );
+            }
+
+            $codeText .= $tabs.'$xml_'.$xmlvarname.' = $xml->createElement(\''.$xmlnodename.'\''.$nodeVal.');'.$breakType;
+            $codeText .= $attCodeText;
+            $codeText .= $nestedCodeText;
+
+            $codeText .= $start_tabs.$parentNodeName.'->appendChild($xml_'.$xmlvarname.');'.$breakType;
+        }
+
+        return $codeText;
+    }
 }
