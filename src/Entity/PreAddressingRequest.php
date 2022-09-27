eee251dbc67ba35f8bc7137525d839bab4cd2c24
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2022-09-27 15:14:58 +0200

    Add entity PreAddressingRequest

---
+++ src/Entity/PreAddressingRequest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\DispatchBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get" = {
+ *             "security" = "is_granted('IS_AUTHENTICATED_FULLY') and is_granted('ROLE_SCOPE_DISPATCH')",
+ *             "path" = "/dispatch/pre-addressing-requests",
+ *             "openapi_context" = {
+ *                 "tags" = {"Dispatch"}
+ *             },
+ *         }
+ *     },
+ *     itemOperations={
+ *         "get" = {
+ *             "security" = "is_granted('IS_AUTHENTICATED_FULLY') and is_granted('ROLE_SCOPE_DISPATCH')",
+ *             "path" = "/dispatch/pre-addressing-requests/{identifier}",
+ *             "openapi_context" = {
+ *                 "tags" = {"Dispatch"}
+ *             },
+ *         },
+ *     },
+ *     iri="https://schema.org/Action",
+ *     shortName="DispatchPreAddressingRequest",
+ *     normalizationContext={
+ *         "groups" = {"DispatchPreAddressingRequest:output"},
+ *         "jsonld_embed_context" = true
+ *     },
+ *     denormalizationContext={
+ *         "groups" = {"DispatchPreAddressingRequest:input"},
+ *         "jsonld_embed_context" = true
+ *     }
+ * )
+ */
+class PreAddressingRequest
+{
+    /**
+     * @ApiProperty(identifier=true)
+     * @Groups({"DispatchPreAddressingRequest:output"})
+     */
+    private $identifier;
+
+    /**
+     * @Groups({"DispatchPreAddressingRequest:output"})
+     */
+    private $recipients;
+
+    public function __construct()
+    {
+        $this->recipients = new ArrayCollection();
+    }
+
+    public function getIdentifier(): string
+    {
+        return (string) $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function getRecipients()
+    {
+        return $this->recipients;
+    }
+}
