19a616fad6baf07ecd0c13f05b50e1edc18df947
Patrizio Bekerle <patrizio@bekerle.com>
2022-10-07 13:02:05 +0200

    Fix linter issues

--- tools/wsdl2php/convert.php
+++ tools/wsdl2php/convert.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 require_once 'vendor/autoload.php';
 
 $generator = new \Wsdl2PhpGenerator\Generator();
 $generator->generate(
-    new \Wsdl2PhpGenerator\Config(array(
+    new \Wsdl2PhpGenerator\Config([
         'inputFile' => './dispatch.wsdl',
-        'outputDir' => './output'
-    ))
+        'outputDir' => './output',
+    ])
 );
