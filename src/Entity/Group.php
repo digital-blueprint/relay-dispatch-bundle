aadc0782a2983f1c9284af36adf27efd5578cc86
Christoph Reiter <reiter.christoph@gmail.com>
2022-10-17 11:32:32 +0200

    Add a dummy groups entity
    As a starting point for introducing user groups

---
+++ src/Entity/Group.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\DispatchBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *         "get" = {
+ *             "path" = "/dispatch/groups",
+ *             "security" = "is_granted('IS_AUTHENTICATED_FULLY') and is_granted('ROLE_SCOPE_DISPATCH')",
+ *             "openapi_context" = {
+ *                 "tags" = {"Dispatch"},
+ *                 "parameters" = {
+ *                     {"name" = "search", "in" = "query", "description" = "Search filter (partial, case-insensitive text search on 'name' attribute)", "type" = "string", "required" = false},
+ *                     {"name" = "lang", "in" = "query", "description" = "Language of result", "type" = "string", "enum" = {"de", "en"}, "example" = "de"},
+ *                 }
+ *             }
+ *         }
+ *     },
+ *     itemOperations={
+ *         "get" = {
+ *             "path" = "/dispatch/groups/{identifier}",
+ *             "security" = "is_granted('IS_AUTHENTICATED_FULLY') and is_granted('ROLE_SCOPE_DISPATCH')",
+ *             "openapi_context" = {
+ *                 "tags" = {"Dispatch"},
+ *                 "parameters" = {
+ *                     {"name" = "identifier", "in" = "path", "description" = "Resource identifier", "required" = true, "type" = "string", "example" = "1190"},
+ *                     {"name" = "lang", "in" = "query", "description" = "Language of result", "type" = "string", "enum" = {"de", "en"}, "example" = "de"},
+ *                 }
+ *             }
+ *         },
+ *     },
+ *     iri="http://schema.org/Organization",
+ *     shortName="DispatchGroup",
+ *     description="A group",
+ *     normalizationContext={
+ *         "jsonld_embed_context" = true,
+ *         "groups" = {"DispatchGroup:output"}
+ *     }
+ * )
+ */
+class Group
+{
+    /**
+     * @ApiProperty(identifier=true)
+     * @Groups({"DispatchGroup:output"})
+     *
+     * @var string
+     */
+    private $identifier;
+
+    /**
+     * @ApiProperty(iri="https://schema.org/name")
+     * @Groups({"DispatchGroup:output"})
+     *
+     * @var string
+     */
+    private $name;
+
+    public function setIdentifier(string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
