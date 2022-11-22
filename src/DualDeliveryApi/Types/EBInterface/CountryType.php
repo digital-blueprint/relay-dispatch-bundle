d7fef25c207ab3c63b119320e55ebed5124ed6e8
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2022-11-22 14:24:25 +0100

    Fix includes after refactoring

--- src/DualDeliveryApi/Types/EBInterface/CountryType.php
+++ src/DualDeliveryApi/Types/EBInterface/CountryType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\DispatchBundle\DualDeliveryApi\Types\EBInterface;
 
+use Dbp\Relay\DispatchBundle\DualDeliveryApi\Types\CountryCodeType;
+
 class CountryType
 {
     /**
