b2491c41f834b3326da4a1598ebd3b316a666925
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-10 13:46:14 +0100

    Add missing function humanFileSize

--- src/Helpers/Tools.php
+++ src/Helpers/Tools.php
@@ -99,4 +99,12 @@ class Tools
     {
         return sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('dispatch_').'.'.$extension;
     }
+
+    public static function humanFileSize($bytes, $decimals = 2): string
+    {
+        $size = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        $factor = floor((strlen((string) $bytes) - 1) / 3);
+
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)).@$size[$factor];
+    }
 }
