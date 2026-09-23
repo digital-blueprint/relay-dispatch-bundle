e8766bea0d90e79707fdd5d08dd67ced1a5b61cd
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-23 12:53:13 +0200

    Port to CoreTestKernelTrait

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,57 +4,29 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\DispatchBundle\Tests;
 
-use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Dbp\Relay\BasePersonBundle\DbpRelayBasePersonBundle;
-use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
+use Dbp\Relay\CoreBundle\TestUtils\CoreTestKernelTrait;
 use Dbp\Relay\DispatchBundle\DbpRelayDispatchBundle;
 use Dbp\Relay\DispatchBundle\DependencyInjection\DbpRelayDispatchExtension;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Nelmio\CorsBundle\NelmioCorsBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+    use CoreTestKernelTrait;
 
-    public function registerBundles(): iterable
+    protected function registerAdditionalBundles(): iterable
     {
-        yield new FrameworkBundle();
-        yield new SecurityBundle();
-        yield new TwigBundle();
-        yield new NelmioCorsBundle();
-        yield new MonologBundle();
         yield new DoctrineBundle();
         yield new DoctrineMigrationsBundle();
-        yield new ApiPlatformBundle();
         yield new DbpRelayBasePersonBundle();
         yield new DbpRelayDispatchBundle();
-        yield new DbpRelayCoreBundle();
     }
 
-    protected function configureRoutes(RoutingConfigurator $routes)
+    protected function configureAdditionalContainer(ContainerConfigurator $container): void
     {
-        $routes->import('@DbpRelayCoreBundle/Resources/config/routing.yaml');
-    }
-
-    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader)
-    {
-        $container->import('@DbpRelayCoreBundle/Resources/config/services_test.yaml');
-        $container->extension('framework', [
-            'test' => true,
-            'secret' => 'something',
-            'annotations' => false,
-        ]);
-
         $container->extension('dbp_relay_core', [
             'queue_dsn' => 'doctrine://'.DbpRelayDispatchExtension::DISPATCH_DB_CONNECTION_ID,
         ]);
